9a8ff46b1e6483b3d2746b0213c7c04d1b81df4e
Riddhi Savsani <w0862829@myscc.ca>
2025-03-27 10:43:22 -0400

    modified header.ts

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,8 +1,4 @@
-import {Component, ViewChild} from '@angular/core';
-import {NgOptimizedImage} from '@angular/common';
-import {MatToolbar} from '@angular/material/toolbar';
-import {MatIcon} from '@angular/material/icon';
-import {MatSidenav, MatSidenavContainer} from '@angular/material/sidenav';
+import {Component} from '@angular/core';
 import {RouterLink, RouterLinkActive} from '@angular/router';
 
 @Component({
@@ -17,19 +13,6 @@ import {RouterLink, RouterLinkActive} from '@angular/router';
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
-  toggleMenu(): void {
-    const menu = document.getElementById("menu");
-    if (menu) {
-      menu.classList.toggle("show");
-    } else {
-      console.error("Menu element not found.");
-    }
-  }
 
-  @ViewChild('sidenav') sidenav!: MatSidenav;  // Reference the sidenav
-
-  closeSidenav() {
-    this.sidenav.close(); // Closes the sidebar when called
-  }
 
 }
