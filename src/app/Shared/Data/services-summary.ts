91fe7e0c455675919fa8e75f32c3479469f1d445
Mohamed Muslemani <w0865765@myscc.ca>
2025-04-12 14:59:40 -0400

    Fix service-details.component and project-detail.component

--- src/app/Shared/Data/services-summary.ts
+++ src/app/Shared/Data/services-summary.ts
@@ -3,19 +3,19 @@ export const SERVICES_SUMMARY = [
   {
     id: 1,
     title: 'Preconstruction Planning',
-    image: 'assets/images/serviceImage1.png',
+    image: 'assets/images/WorkersPipes.jpeg',
     description: 'Effective pre-construction services and planning is the cornerstone of a successful construction project.'
   },
   {
     id: 2,
     title: 'Project Management',
-    image: 'assets/images/serviceImage2.png',
+    image: 'assets/images/WorkerOnLift.jpeg',
     description: 'Comprehensive project management services that ensure every aspect of the project is meticulously planned and executed.'
   },
   {
     id: 3,
     title: 'Construction Consulting',
-    image: 'assets/images/serviceImage3.png',
+    image: 'assets/images/WareHouse.jpeg',
     description: 'Expert construction consulting services to provide guidance and support throughout the entire project lifecycle.'
   }
 ];
